513771736a7323634353fd2e47ef878c1b278b40
steinkorsveien <steinkorsveien@localhost>
2019-11-15 08:54:50 +0100

    added eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React,{Component} from 'react';
-import './App.css';
+import React, { Component } from 'react' /* eslint-disable-line no-unused-vars */
+import './App.css'
 
 class App extends Component {
-
-   render() { return (
-       <div dataTest= "component-app">  
-         <h1 id= "counter" dataTest= "display-count">0</h1>
-         <button id= "button" dataTest= "button-increment" >increment count</button>
-       </div>
-      )
-   }
+  render () {
+    return (
+      <div dataTest= "component-app">
+        <h1 id= "counter" dataTest= "display-count">0</h1>
+        <button id= "button" dataTest= "button-increment" >increment count</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import EzymeAdapter from  'enzyme-adapter-react-16';
-import App from './App';
-import {shallow} from 'enzyme'
 
+import Enzyme, { shallow } from 'enzyme'
+import EzymeAdapter from 'enzyme-adapter-react-16'
+import App from './App' /* eslint-disable-line no-unused-vars */
 
-Enzyme.configure({adapter: new EzymeAdapter()});
+Enzyme.configure({ adapter: new EzymeAdapter() })
 
 it('renders without crashing', () => {
   const wrapper = shallow(<App />)
-  const appComp = wrapper.find({ dataTest: 'component-app'})
+  const appComp = wrapper.find({ dataTest: 'component-app' })
   expect(appComp.length).toBe(1)
 })
 
-it('should exist a increment buttom',()=>{ 
+it('should exist a increment buttom', () => {
   const wrapper = shallow(<App />)
-  const button = wrapper.find({ dataTest: 'button-increment'})
+  const button = wrapper.find({ dataTest: 'button-increment' })
   expect(button.length).toBe(1)
 })
 
-it('should exist a  counter display',()=>{ 
+it('should exist a  counter display', () => {
   const wrapper = shallow(<App />)
-  const display = wrapper.find({ dataTest: 'display-count'})
+  const display = wrapper.find({ dataTest: 'display-count' })
   expect(display.length).toBe(1)
 })
 
-it('should coinatin 1 in counter display',()=>{ })
+it('should coinatin 1 in counter display', () => { })
 
-it('shold contain 1 in counter display ',()=>{ })
-
-
+it('shold contain 1 in counter display ', () => { })
