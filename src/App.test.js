d29f730bf0a14eb5ee02ea69be6b51aed5128e32
steinkorsveien <steinkorsveien@localhost>
2019-11-15 09:02:05 +0100

    added import React

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 
+import React from 'react' /* eslint-disable-line no-unused-vars */
 import Enzyme, { shallow } from 'enzyme'
 import EzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App' /* eslint-disable-line no-unused-vars */
